a472dd36d7c51f6a06727bd01b687a3413a4797c
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-11-22 15:44:22 -0300

    Requisito 01 - feito

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './utility/renderWithRouter';
+import App from '../App';
+
+describe('Testa o componente <App.js />', () => {
+  test('É exibido na tela um link com o texto Home', () => {
+    renderWithRouter(<App />);
+    const checkHome = screen.getByRole('link', { name: /home/i });
+    expect(checkHome).toBeInTheDocument();
+  });
+
+  test('É exibido na tela um link com o texto About', () => {
+    renderWithRouter(<App />);
+    const checkAbout = screen.getByRole('link', { name: /about/i });
+    expect(checkAbout).toBeInTheDocument();
+  });
+
+  test('É exibido na tela um link com o texto Favorite Pokémon', () => {
+    renderWithRouter(<App />);
+    const checkFavorite = screen.getByRole('link', { name: /favorite pokémon/i });
+    expect(checkFavorite).toBeInTheDocument();
+  });
+});
