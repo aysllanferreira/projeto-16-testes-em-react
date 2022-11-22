ee8144f988bc076bcad5653fb01d5dc8da9bb3e7
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-11-22 15:58:05 -0300

    Requisito 03 - feito

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './utility/renderWithRouter';
+import FavoritePokemons from '../pages/FavoritePokemon';
+import App from '../App';
+
+describe('Testa o componente <FavoritePokemons.js />', () => {
+  test('Teste se é exibida na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const checkMessage = screen.getByText(/No favorite Pokémon found/i);
+    expect(checkMessage).toBeInTheDocument();
+  });
+
+  test('Teste se é exibido todos os cards de pokémons favoritados', () => {
+    renderWithRouter(<App />);
+    const checkLink = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(checkLink);
+    const checkFavorite = screen.getByRole('checkbox', { name: /Pokémon favoritado?/i });
+    userEvent.click(checkFavorite);
+    const checkFavoriteLink = screen.getByRole('link', { name: /Favorite Pokémon/i });
+    userEvent.click(checkFavoriteLink);
+    const checkPokemon = screen.getByText(/Pikachu/i);
+    expect(checkPokemon).toBeInTheDocument();
+  });
+});
